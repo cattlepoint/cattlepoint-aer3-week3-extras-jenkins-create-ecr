a99178d09f2adf189a8735593281cf29066afda1
cattlepoint <nobody@nowhere.local>
2025-06-10 13:40:16 -0500

    !

--- create-ecr-user.py
+++ create-ecr-user.py
@@ -2,42 +2,46 @@
 import boto3
 import botocore
 
-stack_name = 'ECRListCreateUser'
-template_file = 'create-ecr-user.yaml'
+STACK_NAME = "ECRListCreateUser"
+TEMPLATE_FILE = "create-ecr-user.yaml"
 
-cf = boto3.client('cloudformation')
+cf = boto3.client("cloudformation")
 
-with open(template_file) as f:
-    template_body = f.read()
+with open(TEMPLATE_FILE) as f:
+    TEMPLATE_BODY = f.read()
 
 def deploy():
     try:
         cf.create_stack(
-            StackName=stack_name,
-            TemplateBody=template_body,
-            Capabilities=['CAPABILITY_NAMED_IAM']
+            StackName=STACK_NAME,
+            TemplateBody=TEMPLATE_BODY,
+            Capabilities=["CAPABILITY_NAMED_IAM"],
         )
-        waiter = cf.get_waiter('stack_create_complete')
+        waiter = cf.get_waiter("stack_create_complete")
     except botocore.exceptions.ClientError as e:
         if 'AlreadyExistsException' in str(e):
             cf.update_stack(
-                StackName=stack_name,
-                TemplateBody=template_body,
-                Capabilities=['CAPABILITY_NAMED_IAM']
+                StackName=STACK_NAME,
+                TemplateBody=TEMPLATE_BODY,
+                Capabilities=["CAPABILITY_NAMED_IAM"],
             )
-            waiter = cf.get_waiter('stack_update_complete')
-        elif 'No updates are to be performed' in str(e):
+            waiter = cf.get_waiter("stack_update_complete")
+        elif "No updates are to be performed" in str(e):
             return
         else:
             raise
-    waiter.wait(StackName=stack_name)
+    waiter.wait(StackName=STACK_NAME)
 
 def fetch_keys():
-    outputs = cf.describe_stacks(StackName=stack_name)['Stacks'][0]['Outputs']
+    outputs = cf.describe_stacks(StackName=STACK_NAME)["Stacks"][0]["Outputs"]
     kv = {o['OutputKey']: o['OutputValue'] for o in outputs}
     print('AccessKeyId:', kv['AccessKeyId'])
     print('SecretAccessKey:', kv['SecretAccessKey'])
 
-if __name__ == '__main__':
+def main() -> None:
     deploy()
     fetch_keys()
+
+
+if __name__ == "__main__":
+    main()
